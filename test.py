5970af961758a59b6ab04ddd6cfb2c1df16b9cae
ffletcherr <homayouneemoradi@gmail.com>
2025-11-24 21:44:42 +0330

    fix format

--- test.py
+++ test.py
@@ -1,12 +1,12 @@
 class Pet:
-    def __init__(self, name, age, category, hunger_bar,favorite_food):
+    def __init__(self, name, age, category, hunger_bar, favorite_food):
         self.name = name
         self.age = age
         self.category = category
         self.hunger_bar = hunger_bar
         self.favorite_food = favorite_food
 
-    def feed(self, food,favorite_food):
+    def feed(self, food, favorite_food):
         if food == "fruit":
             self.hunger_bar = self.hunger_bar + 1
         elif food == "meat":
@@ -21,18 +21,20 @@ class Pet:
         if food == favorite_food:
             self.hunger_bar = self.hunger_bar + 4
 
+
 garfield = Pet(name="garfield", age=2, category="cat", hunger_bar=0, favorite_food="lasania")
 penny = Pet(name="penny", age=8, category="cat", hunger_bar=7, favorite_food="meat")
-snow = Pet(name="snow", age=6, category="dog", hunger_bar=2,favorite_food="fruit")
+snow = Pet(name="snow", age=6, category="dog", hunger_bar=2, favorite_food="fruit")
 
 pets = [garfield, penny, snow]
 pets_favorite_food = ["lasania", "meat", "fruit"]
+
 
 def feed_hunger_pets(pets, critical_hunger_level, food):
     for pet in pets:
         if pet.hunger_bar < critical_hunger_level:
             print(f"{pet.name} is hungry! its hunger bar is: {pet.hunger_bar}")
-            pet.feed(food=food,favorite_food=pet.favorite_food)
+            pet.feed(food=food, favorite_food=pet.favorite_food)
             print(f"I feed {pet.name}. now its hunger bar is: {pet.hunger_bar}")
 
 
